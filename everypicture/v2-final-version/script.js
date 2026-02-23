c2a02ff3c132490d579ee1d278354fc4b71952d5
chao_li <cchli@ucdavis.edu>
2026-02-22 21:27:04 -0800

    2/22

--- everypicture/v2-final-version/script.js
+++ everypicture/v2-final-version/script.js
@@ -13,11 +13,21 @@
         images[i].addEventListener("mouseenter", function() {
             desc.style.display = "block";
             hint.style.display = "none";
+            for (let j = 0; j < images.length; j++) {
+                if (images[j] !== images[i]) {
+                    images[j].style.opacity = "0.1";
+                }
+            }
         });
 
         images[i].addEventListener("mouseleave", function() {
             desc.style.display = "none";
             hint.style.display = "block";
+            for (let j = 0; j < images.length; j++) {
+                if (images[j] !== images[i]) {
+                    images[j].style.opacity = "1";
+                }
+            }
         });
         }
     }
